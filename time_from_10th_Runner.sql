414825c940501ed6da1c13808a81362574551a99
A . Hemanth Sai Manikanta <133625674+aHemanth123@users.noreply.github.com>
2025-03-12 14:10:11 +0530

    Add files via upload

---
+++ time_from_10th_Runner.sql
@@ -0,0 +1,347 @@
+
+
+--Difference Between Times
+
+
+--Last Updated: March 2025
+
+--EY
+--Tata Consultancy
+--Deloitte
+--Medium
+--ID 2064 
+use gender 
+
+CREATE TABLE marathon_mal (
+    place BIGINT,
+    div_tot VARCHAR(10),
+    num BIGINT,
+    person_name VARCHAR(100),
+    age BIGINT,
+    hometown VARCHAR(100),
+    pace BIGINT,
+    gun_time BIGINT,
+    net_time BIGINT
+);
+ 
+ 
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (1, '1/48', 9, 'Joshua Kemei', 22, 'Kenya .', 278, 1728, 1727);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (2, '2/48', 35, 'George Kirwa Misoi', 22, 'Kenya .', 282, 1728, 1727);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (3, '3/48', 3, 'Jacob Frey', 24, 'Oakton VA', 286, 1778, 1777);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (4, '1/96', 49, 'Gurmessa Kumsa', 27, 'Oxon Hill MD', 288, 1786, 1785);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (5, '4/48', 31, 'Mike Baird', 24, 'Ardmore PA', 295, 1874, 1878);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (6, '5/48', 1662, 'Jon Lauder', 23, 'Oakton VA', 302, 1872, 1879);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (7, '1/143', 11, 'Paul Rades', 33, 'Wheaton MD', 306, 1900, 1898);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (8, '6/48', 48, 'Haile Gelalcha', 20, 'Oxon Hill MD', 306, 1899, 1899);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (9, '2/96', 33, 'John Bitok', 26, 'Kenya .', 309, 1915, 1915);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (10, '1/45', 50, 'Dema Woyessa', 19, 'Oxon Hill MD', 314, 1950, 1950);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (11, '7/48', 711, 'Paris Caballero', 23, 'Gaithersburg MD', 314, 1953, 1951);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (12, '2/143', 3011, 'Troy Harrison', 30, 'Hustontown PA', 318, 1974, 1951);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (13, '3/143', 27, 'Albert Haan', 32, 'Washington DC', 318, 1977, 1976);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (14, '1/203', 825, 'Mike Colaiacovo', 36, 'Baltimore MD', 319, 1981, 1980);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (15, '1/201', 15, 'Mark Malander', 48, 'Herndon VA', 320, 1987, 1986);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (16, '2/203', 2299, 'Stavros Rologas', 37, 'Baltimore MD', 321, 1990, 1990);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (17, '2/45', 565, 'Nick Bax', 15, 'Bethesda MD', 322, 2001, 1998);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (18, '8/48', 405, 'Alvin Yew', 20, 'Silver Spring MD', 324, 2012, 2011);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (19, '4/143', 2656, 'Jason Tripp', 33, 'Ellicott City MD', 325, 2019, 2017);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (20, '1/132', 2135, 'Terry Permar', 51, 'Perkasie PA', 326, 2022, 2022);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (21, '3/203', 3013, 'David Mead', 37, 'Washington DC', 328, 2036, 2033);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (22, '1/214', 5, 'Darrell General', 40, 'Bowie MD', 329, 2043, 2042);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (23, '2/214', 1302, 'David Haaga', 44, 'Rockville MD', 330, 2051, 2050);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (24, '5/143', 3019, 'Andrew Cherviyot', 34, 'Beltsville MD', 332, 2064, 2063);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (25, '2/201', 41, 'George Buckheit', 48, 'Reson VA', 332, 2065, 2063);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (26, '3/45', 416, 'David Bernstein', 19, 'Chevy Chase MD', 332, 2064, 2063);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (27, '3/96', 1459, 'Andrew Jacobson', 28, 'Washington DC', 333, 2069, 2067);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (28, '2/132', 17, 'Dan Lawson', 50, 'Gaithersburg MD', 333, 2069, 2069);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (29, '4/45', 410, 'Cooper Linde', 18, 'York PA', 334, 2072, 2070);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (30, '5/45', 897, 'Neal Darmody', 16, 'Gaithersburg MD', 335, 2080, 2078);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (31, '3/214', 2628, 'Derik Thomas', 40, 'Alexandria VA', 335, 2082, 2081);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (32, '4/96', 1791, 'Stephen Majkrzak', 26, 'Olney MD', 339, 2104, 2102);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (33, '4/214', 1632, 'Ronald Kulik', 42, 'Herndon VA', 341, 2115, 2114);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (34, '6/143', 1552, 'David Kent', 34, 'Arlington VA', 341, 2118, 2116);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (35, '6/45', 3227, 'Sam Epstein', 16, 'Bethesda MD', 343, 2129, 2128);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (36, '9/48', 409, 'Justin Ahn', 20, 'Silver Spring MD', 343, 2129, 2128);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (37, '5/214', 922, 'Sheldon Degenhardt', 41, 'Marriottsville V', 343, 2131, 2128);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (38, '5/96', 3012, 'Craig Shearer', 25, 'Gaithersburg MD', 343, 2131, 2128);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (39, '7/143', 997, 'Steven Dowler', 31, 'Kensington MD', 343, 2134, 2130);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (40, '10/48', 576, 'William Bedford', 23, 'Bethesda MD', 343, 2137, 2131);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (41, '6/96', 3346, 'Ted Bradley', 27, 'Comus MD', 344, 2137, 2133);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (42, '6/214', 800, 'Antonio Claro', 44, 'Rockville MD', 344, 2138, 2137);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (43, '11/48', 418, 'Alex Hooke', 22, 'College Park MD', 345, 2141, 2140);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (44, '3/201', 1401, 'Marty Horan', 46, 'Gaithersburg MD', 346, 2148, 2146);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (45, '4/203', 1354, 'Stephen Hays', 38, 'University Park', 346, 2148, 2146);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (46, '8/143', 13, 'Christoph Dean', 31, 'Alexandria VA', 347, 2155, 2153);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (47, '7/96', 1084, 'Thomas Faucette', 29, 'Silver Spring MD', 348, 2165, 2162);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (48, '7/45', 2637, 'John O Thornton', 17, 'Columbia MD', 349, 2170, 2167);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (49, '12/48', 415, 'Paul Longoria', 20, 'College Park MD', 350, 2172, 2170);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (50, '7/214', 1457, 'Paul Jacobson', 43, 'Gaithersburg MD', 350, 2174, 2172);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (51, '4/201', 3242, 'Dennis Coleman', 48, 'Gaithersburg MD', 351, 2179, 2179);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (52, '13/48', 401, 'Aaron Macgray', 21, 'College Park MD', 351, 2183, 2181);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (53, '8/96', 1728, 'Leyi Lin', 26, 'Washington DC', 352, 2190, 2188);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (54, '8/45', 2096, 'Artem Panasenkov', 16, 'Gaithersburg MD', 354, 2202, 2199);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (55, '3/132', 896, 'Peter Darmody', 50, 'Gaithersburg MD', 355, 2203, 2201);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (56, '8/214', 1370, 'Ronald Hess', 44, 'Baltimore MD', 356, 2216, 2213);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (57, '9/96', 1548, 'Kevin Kemp', 27, 'Reston VA', 358, 2226, 2223);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (58, '5/201', 127, 'John Chall', 48, 'Jessup MD', 359, 2228, 2226);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (59, '6/201', 3292, 'Jack Morrison', 46, '.', 359, 2234, 2230);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (60, '9/45', 411, 'Mike Montano', 19, 'Rockville MD', 360, 2232, 2232);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (61, '9/214', 3216, 'Jonathan Auerbach', 42, 'Bethesda MD', 361, 2240, 2238);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (62, '1/73', 442, 'Richard Adams', 55, 'Herndon VA', 361, 2244, 2242);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (63, '7/201', 1293, 'Rafael Guijarro', 45, 'Pembroke Pines F', 362, 2250, 2248);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (64, '8/201', 3241, 'Alvino Castro', 46, 'Brookeville MD', 362, 2250, 2249);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (65, '10/214', 147, 'Terry Eickel', 44, 'Germantown MD', 362, 2254, 2250);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (66, '2/73', 1961, 'Timothy Morgan', 55, 'Damascus MD', 363, 2252, 2250);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (67, '5/203', 1845, 'Tom Matzke', 35, 'Rockville MD', 363, 2257, 2254);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (68, '1/50', 19, 'Patrick Griffith', 61, 'Alexandria VA', 364, 2259, 2259);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (69, '9/201', 3321, 'Oscar Ramirez', 48, 'Gaithersburg MD', 365, 2269, 2265);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (70, '9/143', 2657, 'Ken Trombatore', 33, 'Germantown MD', 366, 2274, 2270);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (71, '3/73', 29, 'Jim Wright', 56, 'Gaithersburg MD', 367, 2278, 2275);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (72, '4/73', 2327, 'Steve Ruckert', 56, 'Woodbine MD', 367, 2279, 2278);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (73, '10/96', 1015, 'Michael Dusenbery', 27, 'Savage MD', 367, 2279, 2278);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (74, '10/201', 1871, 'Weems Mcfadden', 48, 'Laurel MD', 367, 2281, 2280);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (75, '11/214', 1387, 'John Holland', 41, 'Philadelphia PA', 368, 2286, 2285);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (76, '10/45', 2061, 'Burke Oleszewski', 18, 'Bethesda MD', 368, 2288, 2287);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (77, '10/143', 2230, 'Chris Rankin', 34, 'Clarksburg MD', 368, 2292, 2287);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (78, '12/214', 2343, 'Alan Ryder', 42, 'Gaithersburg MD', 369, 2296, 2293);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (79, '4/132', 2725, 'Larry Washington', 54, 'Rockville MD', 369, 2296, 2293);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (80, '6/203', 3255, 'Kevin Odons', 36, 'Rockville MD', 370, 2302, 2297);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (81, '11/45', 2069, 'William Orndorff', 15, 'Rockville MD', 370, 2359, 2298);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (82, '11/201', 2985, 'Bill Stahr', 45, '.', 371, 2305, 2302);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (83, '11/143', 2757, 'Gregory Werner', 30, 'Glen Burnie MD', 372, 2311, 2308);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (84, '7/203', 2477, 'Keith Slonski', 36, 'Gaithersburg MD', 372, 2315, 2311);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (85, '12/201', 1274, 'Jeff Green', 46, 'Rockville MD', 372, 2314, 2311);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (86, '13/201', 2011, 'Greg Nelson', 45, 'Olney MD', 372, 2314, 2311);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (87, '5/132', 1867, 'Denis Mcdonald', 51, 'Woodbine MD', 374, 2323, 2319);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (88, '12/143', 2946, 'John Howard', 34, 'Brunswick MD', 375, 2327, 2325);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (89, '13/214', 835, 'Carleton Conant', 44, 'Kensington MD', 376, 2339, 2336);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (90, '14/48', 404, 'Erik Westlund', 21, 'College Park MD', 377, 2339, 2337);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (91, '14/214', 2192, 'Matthew Prineas', 41, 'Silver Spring MD', 377, 2347, 2343);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (92, '6/132', 2103, 'Joseph Parker', 50, 'Rockville MD', 378, 2351, 2347);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (93, '8/203', 1101, 'Dante Figueroa', 37, 'Washington DC', 378, 2413, 2348);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (94, '9/203', 2057, 'Frank Odeh', 37, 'Oak Hill VA', 380, 2365, 2362);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (95, '7/132', 3249, 'Vassili Triantos', 51, 'Rockville MD', 381, 2367, 2364);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (96, '10/203', 2763, 'Chris Doe', 37, 'Rockville MD', 382, 2375, 2370);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (97, '11/203', 178, 'Mark Krulikowski', 37, 'Arlington VA', 382, 2374, 2371);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (98, '12/45', 2587, 'Matthew Sushinsky', 15, 'Rockville MD', 383, 2379, 2375);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (99, '13/143', 2496, 'Tyson Smith', 31, 'Washington DC', 383, 2385, 2380);
+
+INSERT INTO marathon_mal (place, div_tot, num, person_name, age, hometown, pace, gun_time, net_time)
+VALUES (100, '15/48', 402, 'Nick Fernandez', 22, 'College Park MD', 384, 2389, 2383);
+ 
+select * from marathon_mal; 
+ 
+
+-- In a marathon, gun time is counted from the moment of the formal start of the race while net time is counted from the moment a runner crosses a starting line. Both variables are in seconds.
+--How much net time separates Chris Doe from the 10th best net time (in ascending order)? Avoid gaps in the ranking calculation. Output absolute net time difference.
+
+WITH RankedRunners AS (
+    SELECT
+        person_name,
+        net_time,
+        DENSE_RANK() OVER (ORDER BY net_time ASC ) AS net_time_rank
+    FROM marathon_mal
+) 
+SELECT  
+    ABS(
+        (SELECT TOP 1 net_time FROM RankedRunners WHERE person_name = 'Chris Doe')
+        -
+        (SELECT TOP 1 net_time FROM RankedRunners WHERE net_time_rank = 10)
+    ) AS absolute_net_time_difference;    --- use should top 1x
